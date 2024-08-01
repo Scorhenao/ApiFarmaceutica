a8e54e71447256743d6e0b2b4d73655da18c6f27
Scorhenao <samcorhenao@gmail.com>
2024-08-01 17:39:52 -0400

    connection with mysql2 to the db

---
+++ src/db/connection.ts
@@ -0,0 +1,19 @@
+// src/db/connection.ts
+import mysql from 'mysql2';
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'farmacia',
+  password: 'Rlwl2023'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database.');
+});
+
+export default connection;
